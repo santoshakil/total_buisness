73f6b2b00338061a8138597f6eab5f10a78a7ff3
Santo Shakil <shakil.mezbah@gmail.com>
2020-09-21 12:45:34 +0600

    first commit

---
+++ lib/products/product_model.dart
@@ -0,0 +1,12 @@
+class ProductModel {
+  final String name, discription, quantityType;
+  final int price, quantity;
+
+  ProductModel({
+    this.name,
+    this.discription,
+    this.quantityType,
+    this.price,
+    this.quantity,
+  });
+}
